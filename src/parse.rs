bf9afd83b1a9fc1a375e583be13f7440d8a20d8f
Tom Tromey <tom@tromey.com>
2016-09-11 11:27:52 -0600

    parse T packet response; plus bug fix

--- src/parse.rs
+++ src/parse.rs
@@ -65,7 +65,158 @@ named!(pub parse_stop_signal<&[u8], u8>,
               ~ eof
               , || value));
 
-// FIXME 'T' packet
+pub enum StopReplyValue {
+    Register(u64, Vec<u8>),
+    Thread(ProcessId),
+    Core(u64),
+    Unknown(Vec<u8>),
+    Watch(u64),
+    Awatch(u64),
+    Rwatch(u64),
+    SyscallEntry(u64),
+    SyscallReturn(u64),
+    LibraryChange,
+    ReplayLog(bool),
+    SoftwareBreak,
+    HardwareBreak,
+    Fork(ProcessId),
+    VFork(ProcessId),
+    VForkDone,
+    Exec(Vec<u8>),
+    Create,
+}
+
+// Helper for T packet.  Parse a register number.
+named!(parse_t_register<&[u8], StopReplyValue>,
+       chain!(regno: parse_hex_number
+              ~ tag!(":")
+              ~ value: parse_hex_data
+              , || StopReplyValue::Register(regno, value)));
+
+// Helper for T packet.  Parse "thread".
+named!(parse_t_thread<&[u8], StopReplyValue>,
+       chain!(tag!("thread:")
+              ~ id: parse_thread_id
+              , || StopReplyValue::Thread(id)));
+
+// Helper for T packet.  Parse "core".
+named!(parse_t_core<&[u8], StopReplyValue>,
+       chain!(tag!("core:")
+              ~ core: parse_hex_number
+              , || StopReplyValue::Core(core)));
+
+// Helper for T packet.  Parse "watch".
+named!(parse_t_watch<&[u8], StopReplyValue>,
+       chain!(tag!("watch:")
+              ~ addr: parse_hex_number
+              , || StopReplyValue::Watch(addr)));
+
+// Helper for T packet.  Parse "awatch".
+named!(parse_t_awatch<&[u8], StopReplyValue>,
+       chain!(tag!("awatch:")
+              ~ addr: parse_hex_number
+              , || StopReplyValue::Awatch(addr)));
+
+// Helper for T packet.  Parse "rwatch".
+named!(parse_t_rwatch<&[u8], StopReplyValue>,
+       chain!(tag!("rwatch:")
+              ~ addr: parse_hex_number
+              , || StopReplyValue::Rwatch(addr)));
+
+// Helper for T packet.  Parse "syscall_entry".
+named!(parse_t_syscall_entry<&[u8], StopReplyValue>,
+       chain!(tag!("syscall_entry:")
+              ~ call: parse_hex_number
+              , || StopReplyValue::SyscallEntry(call)));
+
+// Helper for T packet.  Parse "syscall_return".
+named!(parse_t_syscall_return<&[u8], StopReplyValue>,
+       chain!(tag!("syscall_return:")
+              ~ call: parse_hex_number
+              , || StopReplyValue::SyscallReturn(call)));
+
+// Read until ";" or eof.
+named!(skip_until_semi<&[u8]>,
+       alt_complete!(take_until!(";") | eof));
+
+// Helper for T packet.  Parse "library".
+named!(parse_t_library<&[u8], StopReplyValue>,
+       chain!(tag!("library:")
+              ~ skip_until_semi
+              , || StopReplyValue::LibraryChange));
+
+// Helper for T packet.  Parse "replaylog".
+named!(parse_t_replaylog<&[u8], StopReplyValue>,
+       chain!(tag!("replaylog:")
+              ~ result: alt!(tag!("begin") => { |_| true }
+                             | tag!("end") => { |_| false })
+              , || StopReplyValue::ReplayLog(result)));
+
+// Helper for T packet.  Parse "swbreak".
+named!(parse_t_swbreak<&[u8], StopReplyValue>,
+       value!(StopReplyValue::SoftwareBreak, tag!("swbreak:")));
+
+// Helper for T packet.  Parse "hwbreak".
+named!(parse_t_hwbreak<&[u8], StopReplyValue>,
+       value!(StopReplyValue::HardwareBreak, tag!("hwbreak:")));
+
+// Helper for T packet.  Parse "fork".
+named!(parse_t_fork<&[u8], StopReplyValue>,
+       chain!(tag!("fork:")
+              ~ pid: parse_thread_id
+              , || StopReplyValue::Fork(pid)));
+
+// Helper for T packet.  Parse "vfork".
+named!(parse_t_vfork<&[u8], StopReplyValue>,
+       chain!(tag!("vfork:")
+              ~ pid: parse_thread_id
+              , || StopReplyValue::VFork(pid)));
+
+// Helper for T packet.  Parse "vforkdone".
+named!(parse_t_vforkdone<&[u8], StopReplyValue>,
+       chain!(tag!("vforkdone:")
+              ~ skip_until_semi
+              , || StopReplyValue::VForkDone));
+
+// Helper for T packet.  Parse "exec".
+named!(parse_t_exec<&[u8], StopReplyValue>,
+       chain!(tag!("exec:")
+              ~ path: parse_hex_data
+              , || StopReplyValue::Exec(path)));
+
+// Helper for T packet.  Parse "create".
+named!(parse_t_create<&[u8], StopReplyValue>,
+       chain!(tag!("create:")
+              ~ skip_until_semi
+              , || StopReplyValue::Create));
+
+// Helper for T packet.  Parse any single `T` pair.
+named!(parse_any_t_pair<&[u8], StopReplyValue>,
+       alt_complete!(parse_t_register
+                     | parse_t_thread
+                     | parse_t_core
+                     | parse_t_watch
+                     | parse_t_awatch
+                     | parse_t_rwatch
+                     | parse_t_syscall_entry
+                     | parse_t_syscall_return
+                     | parse_t_library
+                     | parse_t_replaylog
+                     | parse_t_swbreak
+                     | parse_t_hwbreak
+                     | parse_t_fork
+                     | parse_t_vfork
+                     | parse_t_vforkdone
+                     | parse_t_exec
+                     | parse_t_create));
+
+/// Parse a stop-reply long-form signal (`T`) packet.
+named!(pub parse_stop_signal_full<&[u8], (u8, Vec<StopReplyValue>) >,
+       chain!(tag!("T")
+              ~ signo: parse_2_hex
+              ~ values: separated_nonempty_list!(tag!(";"), parse_any_t_pair)
+              ~ eof
+              , || (signo, values)));
 
 /// Parse a stop-reply exit packet.
 named!(pub parse_stop_exit<&[u8], (u8, Option<u64>)>,
@@ -79,9 +230,11 @@ named!(pub parse_stop_exit<&[u8], (u8, Option<u64>)>,
               , || (value, pid)));
 
 /// Parse a stop-reply exited-with-signal packet.
-named!(pub parse_stop_exit_signal<&[u8], (u8, Option<u64>)>,
+named!(pub parse_stop_exit_signal<&[u8], (u64, Option<u64>)>,
        chain!(tag!("X")
-              ~ value: parse_2_hex // FIXME more is ok!!?
+              // The docs say this takes two digits but in reality any
+              // number of digits is accepted.
+              ~ value: parse_hex_number
               ~ pid: opt!(
                   chain!(tag!(";process:")
                          ~ pid: parse_hex_number
@@ -90,9 +243,11 @@ named!(pub parse_stop_exit_signal<&[u8], (u8, Option<u64>)>,
               , || (value, pid)));
 
 /// Parse a stop-reply thread-exited packet.
-named!(pub parse_stop_thread_exit<&[u8], (u8, u64)>,
+named!(pub parse_stop_thread_exit<&[u8], (u64, u64)>,
        chain!(tag!("w")
-              ~ value: parse_2_hex // FIXME more is ok!!?
+              // The docs say this takes two digits but in reality any
+              // number of digits is accepted.
+              ~ value: parse_hex_number
               ~ tag!(";")
               ~ pid: parse_hex_number
               ~ eof
